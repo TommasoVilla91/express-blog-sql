bc448481d98e973166207050df6a7a8769d84a9f
TommasoVilla91 <tommasovilla@yahoo.it>
2025-01-22 15:16:11 +0100

    feat: delete added and tested

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -2,11 +2,13 @@ const connection = require("../data/db")
 
 // get
 const index = (req, res) => {  
+
     // preparare la query
     const sql = 'SELECT * FROM `posts`';
 
     // eseguire la query
     connection.query(sql, (err, results) => {
+        // se trova errore mandare messaggio errore server sennò dare risultati richiesti
         if (err) {
             return res.status(500).json({
                 message: "Errore interno del server"
@@ -42,7 +44,24 @@ const show = (req, res) => {
 
 // delete
 const destroy = (req, res) => {
+
+    // recupero l'id
+    const id = req.params.id;
     
+    // preparare la query
+    const sql = "DELETE FROM `posts` WHERE id = ?";
+
+    // eseguire la query
+    connection.query(sql, [id], (err) => {
+        // se trova errore mandare messaggio errore sennò eliminare elemento richiesto
+        if (err) {
+            return res.status(500).json({
+                message: 'Errore interno del server'
+            });
+        } else {
+            return res.sendStatus(204);
+        };
+    });
 };
 
 module.exports = {
